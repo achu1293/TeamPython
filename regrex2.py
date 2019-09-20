6e868a5c00116183b819fd099432c7bdebf5119a
Archana <39257114+achu1293@users.noreply.github.com>
2019-09-20 13:49:30 +0530

    Update regrex2.py

--- regrex2.py
+++ regrex2.py
@@ -1,15 +1,13 @@
 import re
 #s=[210^"AAA"^"#COIMBATORE"^9876543213,211^"ABC"^"#MADRAS"^9877543213,212^"XYC"^9897543213,213^"#ERODE"^9897543218]
 t=int(input())
-d,l={},[]
+d,l,l1,l2={},[],[],[]
 d1={}
 for n in range(t):
     s=input()
-    m=len(s.split('^'))
-    #for i in s.split(','):    
+    m=len(s.split('^'))        
     for j in s.split('^'):
-        if re.match("^[2]",j):            
-            #d['id']=j
+        if re.match("^[7]",j):       
             a=j
         elif re.findall("^#[a-zA-Z]+",j):            
             d['address']=j        
@@ -17,13 +15,12 @@ for n in range(t):
             d['name']=j        
         elif re.findall("^[7|8|9][0-9]{9}$",j):
             d['mobile']=j
-    #print("id is",d,d['id'])
-    #a=d['id']
+    
     if a not in d1:
         d1[a]=d
     else:
         d1[a].update(d)
-    d={}
+    d={}   
 for i,j in d1.items():
     if m<len(j.keys()):
         m=len(j.keys())
@@ -36,40 +33,18 @@ for i,j in d1.items():
             j[n]="null"     
 #print(d1)
 for i,j in d1.items():
-    print(i,end=" ")
-    for n,m in j.items():
-        print(m,end=" ")
+    l1.extend([i,j['name'],j['address'],j['mobile']])
+    l2.append(l1)
+    l1=[]
+l2=sorted(l2,key=lambda i:i[0])
+#print(l2)
+for i in l2:
+    for j in i:
+        print(j,end="   ")
     print()
-
-#[['raj',23,5678],['ram',45,54768],['sam',34,7658]]
-
-
-'''d3={}
-for i in range(len(l)):
-    k=l[i]['id']    
-    if k not in d3:
-        d3[k]=l[i]        
-    else:
-        d3[k].update(l[i])
-print(d3)
-
-
-print(d)
-    l.append(d)
-    d={}
-print(l)
-m,l1=0,[]
-for i in l:
-    m=len(i)
-    l1.append(m)
-m1=max(l1)
-for i in l:
-    if len(i)==m1:
-        a=i.keys()
-a=list(a)
-m=list(l[0].keys())'''
-
-
 #210^AAA^#COIMBATORE^9876543213,211^ABC^#MADRAS^9877543213,212^XYC^9897543213,212^#ERODE       
+            
+        
+
             
         
